00086e7f24a02519c115a25a93ff70f8c7bcb448
Zachary Mayle <zam1998@gmail.com>
2019-11-15 22:05:11 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+hopper = programmer_hash[:grace_hopper][:known_for][:languages]
 
 end
 
@@ -59,7 +59,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
+# :yukihiro_matsumoto = > {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
